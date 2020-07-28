c670a299cbccf812783defa5b4be5580603a0388
Cm0nsters <cmonstersschroepfer@gmail.com>
2020-07-28 03:11:44 -0500

    add 'search' functionality

---
+++ cogs/search.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+import bs4
+import asyncio
+from googlesearch import search
+
+class Search(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def search(self,ctx,*,args:str):
+        results = []
+        searchResult=search(query=args,tld='com',lang='en',num=3,start=0,stop=3, pause=2)
+        
+        for i in searchResult: 
+            results.append(i)
+        
+        await ctx.send("Here's the top 3 results found for `{}`:".format(args))
+
+        for j in range(3):
+            await ctx.send("<{}>".format(results[j]))
+            
+
+def setup(client):
+    client.add_cog(Search(client))
