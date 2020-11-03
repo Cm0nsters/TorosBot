2ae490f6735bda5f4706b2ce488e92963d797d86
Cm0nsters <cmonstersschroepfer@gmail.com>
2020-11-03 15:34:26 -0600

    update and notes

--- cogs/music.py
+++ cogs/music.py
@@ -7,7 +7,7 @@ import os
 class Music(commands.Cog):
     def __init__(self, client):
         self.client = client
-        self.queue = []
+        self.songQueue = []
         self.queuePos = 1
 
         self.ytdl_opts={
@@ -39,13 +39,19 @@ class Music(commands.Cog):
         if voice and voice.is_connected():
             await voice.disconnect()
 
+#rewrite literally everything underneath this to actually function and be simplified
+
+    @commands.command(aliases=['q'])
+    async def queue(self,ctx):
+        await ctx.send(f"{self.songQueue}\nCurrent Position: {self.queuePos}")
+
     @commands.is_owner()
     @commands.command(aliases=['p'])
     async def play(self, ctx, url):
         channel = ctx.message.author.voice.channel
         voice = get(self.client.voice_clients, guild=ctx.guild)
-        self.queue.append(f"song{self.queuePos}.mp3")
-        self.ytdl_opts.update({'outtmpl': f'./song{self.queuePos}.%(ext)s'})
+        self.songQueue.append(f"song{len(self.songQueue)}.mp3")
+        self.ytdl_opts.update({'outtmpl': f'./song{len(self.songQueue)}.%(ext)s'})
         
         if voice and voice.is_connected():
             await voice.move_to(channel)
@@ -62,12 +68,12 @@ class Music(commands.Cog):
         else:
             voice.play(discord.FFmpegPCMAudio(f"song{self.queuePos}.mp3"))
 
-        #add removal of download and queue system
+    #add removal of download and songQueue system
     @commands.command()
     async def skip(self, ctx):
         voice = get(self.client.voice_clients, guild=ctx.guild)
         if voice and voice.is_connected():
-            self.queue.remove(f'song{self.queuePos}.mp3')
+            self.songQueue.remove(f'song{self.queuePos}.mp3')
             os.remove(f'song{self.queuePos}.mp3')
             self.queuePos += 1
         else:
